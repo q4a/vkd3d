d68e60a78a815b9e3857d5b21c174851e85150ae
Henri Verbeet <hverbeet@codeweavers.com>
2024-05-03 01:11:06 +0200

    vkd3d-shader/glsl: Implement VKD3DSIH_IEQ.

--- libs/vkd3d-shader/glsl.c
+++ libs/vkd3d-shader/glsl.c
@@ -722,6 +722,9 @@ static void vkd3d_glsl_handle_instruction(struct vkd3d_glsl_generator *gen,
         case VKD3DSIH_DP4:
             shader_glsl_dot(gen, ins, VKD3DSP_WRITEMASK_ALL);
             break;
+        case VKD3DSIH_IEQ:
+            shader_glsl_relop(gen, ins, "==", "equal");
+            break;
         case VKD3DSIH_FRC:
             shader_glsl_intrinsic(gen, ins, "fract");
             break;
