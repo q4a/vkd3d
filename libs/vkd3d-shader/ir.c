0edd654c9e93d86f3d4a42f20b8f818055db0e67
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-09-14 17:52:06 +0200

    vkd3d-shader/ir: Validate ADD instructions.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -9996,6 +9996,7 @@ static const struct vsir_validator_instruction_desc vsir_validator_instructions[
 {
     [VKD3DSIH_ABS] =                              {1,   1, vsir_validate_float_elementwise_operation},
     [VKD3DSIH_ACOS] =                             {1,   1, vsir_validate_float_elementwise_operation},
+    [VKD3DSIH_ADD] =                              {1,   2, vsir_validate_float_elementwise_operation},
     [VKD3DSIH_BRANCH] =                           {0, ~0u, vsir_validate_branch},
     [VKD3DSIH_HS_CONTROL_POINT_PHASE] =           {0,   0, vsir_validate_hull_shader_phase},
     [VKD3DSIH_HS_DECLS] =                         {0,   0, vsir_validate_hull_shader_phase},
