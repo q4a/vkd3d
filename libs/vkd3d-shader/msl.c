2669ae16eae0118f6d6e8a1d014d9c5385ce7aaf
Henri Verbeet <hverbeet@codeweavers.com>
2024-11-15 17:17:24 +0100

    vkd3d-shader/msl: Implement VKD3DSIH_DCL_OUTPUT_SIV.

--- libs/vkd3d-shader/msl.c
+++ libs/vkd3d-shader/msl.c
@@ -313,6 +313,7 @@ static void msl_handle_instruction(struct msl_generator *gen, const struct vkd3d
     switch (ins->opcode)
     {
         case VKD3DSIH_DCL_INPUT:
+        case VKD3DSIH_DCL_OUTPUT_SIV:
         case VKD3DSIH_NOP:
             break;
         case VKD3DSIH_MOV:
