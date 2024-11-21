26251d02a9f26fbff1f30cc64bad40f6ff75392f
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-11-21 15:25:04 +0100

    vkd3d-shader/ir: Validate index count for TESSCOORD registers.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -7233,6 +7233,10 @@ static void vsir_validate_register(struct validation_context *ctx,
             vsir_validate_io_register(ctx, reg);
             break;
 
+        case VKD3DSPR_TESSCOORD:
+            vsir_validate_register_without_indices(ctx, reg);
+            break;
+
         case VKD3DSPR_DEPTHOUTGE:
             vsir_validate_register_without_indices(ctx, reg);
             break;
