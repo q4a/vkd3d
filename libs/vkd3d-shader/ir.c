fd9da5db2d986a178b56d18959ef6bab5afeb3a1
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-11-21 15:25:36 +0100

    vkd3d-shader/ir: Validate index count for THREADID registers.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -7237,6 +7237,10 @@ static void vsir_validate_register(struct validation_context *ctx,
             vsir_validate_register_without_indices(ctx, reg);
             break;
 
+        case VKD3DSPR_THREADID:
+            vsir_validate_register_without_indices(ctx, reg);
+            break;
+
         case VKD3DSPR_DEPTHOUTGE:
             vsir_validate_register_without_indices(ctx, reg);
             break;
