050840f49329be1b6cf4dd16b05157123872eace
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-11-21 15:26:42 +0100

    vkd3d-shader/ir: Validate index count for LOCALTHREADID registers.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -7245,6 +7245,10 @@ static void vsir_validate_register(struct validation_context *ctx,
             vsir_validate_register_without_indices(ctx, reg);
             break;
 
+        case VKD3DSPR_LOCALTHREADID:
+            vsir_validate_register_without_indices(ctx, reg);
+            break;
+
         case VKD3DSPR_DEPTHOUTGE:
             vsir_validate_register_without_indices(ctx, reg);
             break;
