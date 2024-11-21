9f3bbad6bc29e5f5f995e39de06d3f050ca704e8
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-11-21 23:01:56 +0100

    vkd3d-shader/ir: Validate index count for COVERAGE registers.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -7253,6 +7253,10 @@ static void vsir_validate_register(struct validation_context *ctx,
             vsir_validate_register_without_indices(ctx, reg);
             break;
 
+        case VKD3DSPR_COVERAGE:
+            vsir_validate_register_without_indices(ctx, reg);
+            break;
+
         case VKD3DSPR_DEPTHOUTGE:
             vsir_validate_register_without_indices(ctx, reg);
             break;
