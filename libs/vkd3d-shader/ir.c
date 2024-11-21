e367dc07833774971c21e0f70696020dad2cf4a7
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-11-21 23:04:06 +0100

    vkd3d-shader/ir: Validate index count for WAVELANEINDEX registers.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -7281,6 +7281,10 @@ static void vsir_validate_register(struct validation_context *ctx,
             vsir_validate_register_without_indices(ctx, reg);
             break;
 
+        case VKD3DSPR_WAVELANEINDEX:
+            vsir_validate_register_without_indices(ctx, reg);
+            break;
+
         default:
             break;
     }
