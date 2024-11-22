f2a32589ea5f0bb25554f102abe3e8b3985e2e11
Feifan He <feifan@codeweavers.com>
2024-11-22 15:45:54 +0800

    vkd3d-shader/msl: Implement VKD3DSIH_INE.

--- libs/vkd3d-shader/msl.c
+++ libs/vkd3d-shader/msl.c
@@ -445,6 +445,7 @@ static void msl_handle_instruction(struct msl_generator *gen, const struct vkd3d
         case VKD3DSIH_DCL_OUTPUT_SIV:
         case VKD3DSIH_NOP:
             break;
+        case VKD3DSIH_INE:
         case VKD3DSIH_NEU:
             msl_relop(gen, ins, "!=");
             break;
