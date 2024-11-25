c22812e20bee533a1a4e1e1f36d24a9d84e8f9f5
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-11-25 15:04:09 +0100

    vkd3d-shader/ir: Validate index count for OUTSTENCILREF registers.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -7273,6 +7273,10 @@ static void vsir_validate_register(struct validation_context *ctx,
             vsir_validate_register_without_indices(ctx, reg);
             break;
 
+        case VKD3DSPR_OUTSTENCILREF:
+            vsir_validate_register_without_indices(ctx, reg);
+            break;
+
         case VKD3DSPR_SSA:
             vsir_validate_ssa_register(ctx, reg);
             break;
