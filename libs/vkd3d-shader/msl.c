568296c88fee1b29fb127d02f4d41c3e1207e3d9
Feifan He <feifan@codeweavers.com>
2024-12-03 00:09:01 +0800

    vkd3d-shader/msl: Implement VKD3DSIH_ROUND_NE.

--- libs/vkd3d-shader/msl.c
+++ libs/vkd3d-shader/msl.c
@@ -624,6 +624,9 @@ static void msl_handle_instruction(struct msl_generator *gen, const struct vkd3d
         case VKD3DSIH_RET:
             msl_ret(gen, ins);
             break;
+        case VKD3DSIH_ROUND_NE:
+            msl_intrinsic(gen, ins, "rint");
+            break;
         case VKD3DSIH_ROUND_NI:
             msl_intrinsic(gen, ins, "floor");
             break;
