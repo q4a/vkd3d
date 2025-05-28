94cbb333f838a42c3ffb2216f6551cf5da684ef8
Giovanni Mascellani <gmascellani@codeweavers.com>
2025-05-28 23:04:25 +0200

    vkd3d-shader/dxil: Emit 16-bit ICB values as minimum precision registers.

--- libs/vkd3d-shader/dxil.c
+++ libs/vkd3d-shader/dxil.c
@@ -2597,6 +2597,7 @@ static void sm6_register_from_value(struct vkd3d_shader_register *reg, const str
             reg->idx[0].offset = value->u.icb.id;
             register_index_address_init(&reg->idx[1], value->u.icb.index.index, sm6);
             reg->idx[1].is_in_bounds = value->u.icb.index.is_in_bounds;
+            register_convert_to_minimum_precision(reg);
             break;
 
         case VALUE_TYPE_IDXTEMP:
