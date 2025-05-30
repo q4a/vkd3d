6bf25800c2431affa0c3c3694286d7aab2d5a0e1
Giovanni Mascellani <gmascellani@codeweavers.com>
2025-05-30 14:38:17 +0200

    vkd3d-shader/ir: Validate DSX_FINE instructions.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -11232,6 +11232,7 @@ static const struct vsir_validator_instruction_desc vsir_validator_instructions[
     [VKD3DSIH_DRCP] =                             {1,   1, vsir_validate_double_elementwise_operation},
     [VKD3DSIH_DSX] =                              {1,   1, vsir_validate_float_elementwise_operation},
     [VKD3DSIH_DSX_COARSE] =                       {1,   1, vsir_validate_float_elementwise_operation},
+    [VKD3DSIH_DSX_FINE] =                         {1,   1, vsir_validate_float_elementwise_operation},
     [VKD3DSIH_HS_CONTROL_POINT_PHASE] =           {0,   0, vsir_validate_hull_shader_phase},
     [VKD3DSIH_HS_DECLS] =                         {0,   0, vsir_validate_hull_shader_phase},
     [VKD3DSIH_HS_FORK_PHASE] =                    {0,   0, vsir_validate_hull_shader_phase},
