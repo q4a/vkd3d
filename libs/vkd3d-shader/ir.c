68ad2becdb34d8307aafef727afa5a3a93363473
Giovanni Mascellani <gmascellani@codeweavers.com>
2025-07-16 21:07:41 +0200

    vkd3d-shader/ir: Validate ISNAN operations.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -11513,6 +11513,7 @@ static const struct vsir_validator_instruction_desc vsir_validator_instructions[
     [VSIR_OP_ISHL] =                             {1,   2, vsir_validate_integer_elementwise_operation},
     [VSIR_OP_ISHR] =                             {1,   2, vsir_validate_integer_elementwise_operation},
     [VSIR_OP_ISINF] =                            {1,   1, vsir_validate_float_comparison_operation},
+    [VSIR_OP_ISNAN] =                            {1,   1, vsir_validate_float_comparison_operation},
     [VSIR_OP_LABEL] =                            {0,   1, vsir_validate_label},
     [VSIR_OP_LOOP] =                             {0, ~0u, vsir_validate_loop},
     [VSIR_OP_NOP] =                              {0,   0, vsir_validate_nop},
