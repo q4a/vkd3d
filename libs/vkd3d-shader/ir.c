q4a/vkd3d

commit dfc18781cc46af636cc49c7cf0b1949589499df9

vkd3d-shader/ir: Check that GSINSTID registers have dimension SCALAR.

libs/vkd3d-shader/ir.c
@@ -9554,6 +9554,7 @@ static void vsir_validate_register(struct validation_context *ctx,
         [VKD3DSPR_LOCALTHREADINDEX] = {true, 0, VSIR_DIMENSION_VEC4},
         [VKD3DSPR_COVERAGE]         = {true, 0, VSIR_DIMENSION_VEC4},
         [VKD3DSPR_SAMPLEMASK]       = {true, 0, VSIR_DIMENSION_SCALAR},
+        [VKD3DSPR_GSINSTID]         = {true, 0, VSIR_DIMENSION_SCALAR},
         [VKD3DSPR_DEPTHOUTGE]       = {true, 0, VSIR_DIMENSION_SCALAR},
         [VKD3DSPR_DEPTHOUTLE]       = {true, 0, VSIR_DIMENSION_SCALAR},
     };
@@ -9689,10 +9690,6 @@ static void vsir_validate_register(struct validation_context *ctx,
             break;
 
         case VKD3DSPR_TESSCOORD:
-            vsir_validate_register_without_indices(ctx, reg);
-            break;
-
-        case VKD3DSPR_GSINSTID:
             vsir_validate_register_without_indices(ctx, reg);
             break;
 

commit 22f84317c01b5b7008e23c17b18f676eef6ccc13

vkd3d-shader/ir: Check that GSINSTID registers have dimension VEC4.
I had set it to SCALAR in an earlier commit, but I hadn't noticed
it failed validation.

Also fix the HLSL compiler to emit them accordingly.

Fixes: dfc18781cc46af636cc49c7cf0b1949589499df9

libs/vkd3d-shader/ir.c
@@ -9592,7 +9592,7 @@ static void vsir_validate_register(struct validation_context *ctx,
         [VKD3DSPR_LOCALTHREADINDEX] = {true, 0, VSIR_DIMENSION_VEC4},
         [VKD3DSPR_COVERAGE]         = {true, 0, VSIR_DIMENSION_VEC4},
         [VKD3DSPR_SAMPLEMASK]       = {true, 0, VSIR_DIMENSION_SCALAR},
-        [VKD3DSPR_GSINSTID]         = {true, 0, VSIR_DIMENSION_SCALAR},
+        [VKD3DSPR_GSINSTID]         = {true, 0, VSIR_DIMENSION_VEC4},
         [VKD3DSPR_DEPTHOUTGE]       = {true, 0, VSIR_DIMENSION_SCALAR},
         [VKD3DSPR_DEPTHOUTLE]       = {true, 0, VSIR_DIMENSION_SCALAR},
         [VKD3DSPR_OUTSTENCILREF]    = {true, 0, VSIR_DIMENSION_SCALAR},

libs/vkd3d-shader/tpf.c
@@ -2237,7 +2237,6 @@ bool shader_sm4_is_scalar_register(const struct vkd3d_shader_register *reg)
         case VKD3DSPR_DEPTHOUT:
         case VKD3DSPR_DEPTHOUTGE:
         case VKD3DSPR_DEPTHOUTLE:
-        case VKD3DSPR_GSINSTID:
         case VKD3DSPR_OUTPOINTID:
         case VKD3DSPR_PRIMID:
         case VKD3DSPR_SAMPLEMASK: